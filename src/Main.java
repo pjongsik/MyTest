412c81ccefdcad944017a8477994a41085e72ece
pjongsik <pjongsik@naver.com>
2019-03-20 14:39:21 +0900

    메인 수정

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 
 public class Main {
-
+	
 	public static void main(String[] args) {
+		System.out.println("MAIN START ---!!");
 		// TODO Auto-generated method stub
 		for (int i=0; i < 100; i++)
 			System.out.println(i);
+		
+		System.out.println("MAIN END ---!!");
 	}
-
+	
 }
